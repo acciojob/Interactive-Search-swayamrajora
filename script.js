041fa462abaf3627a0755cf019496c6e652e4f56
swayamrajora <127737419+swayamrajora@users.noreply.github.com>
2025-05-16 07:59:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+
+const search = document.querySelector('.search');
+const input = document.querySelector('.input');
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+  search.classList.add('active'); 
+  input.focus();                  
+});
